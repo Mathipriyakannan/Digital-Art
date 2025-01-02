f371d50b84091a3ad3367de4b91236dc6f5e7f69
Mathipriya.K <148265131+Mathipriyakannan@users.noreply.github.com>
2025-01-02 22:19:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+ function App() {
+  return(
+<div>
+
+
+
+<Login></Login>
+
+  
+</div>
+  )
+ }
+ export default App
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Login from "./Login";
+import "./ImageGallery.css";
+import "./Login.css";
+import ImageGallery from "./ImageGallery"
+//import {Route, BrowserRouter, Routes, } from 'react-router-dom';
+ function App() {
+  return(
+<div>
+
+
+
+<Login></Login>
+<ImageGallery></ImageGallery>
+
+
+</div>
+  )
+ }
+ export default App;
